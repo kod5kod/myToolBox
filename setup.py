e7cd3e2d6fd316337baee33d98504e05c52f8fe4
kod5kod <liorec@gmail.com>
2017-03-01 18:51:37 -0500

    adding a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'myToolBox',
+    version = '0.1',
+    description='A collection of objects and functions for everyday use',
+    author='kod5kod',
+    author_git = 'https://github.com/kod5kod',
+    packages=['distutils','hashlib'],
+     
+
+)
+
